85d92b96e4f26742ba17f57357c11c2aa0f635a5
Andy Short <andynshort@gmail.com>
2013-09-17 12:25:09 -0700

    configured braintree

---
+++ config/initializers/braintree.rb
@@ -0,0 +1,4 @@
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = "ky393fsx68f4x723"
+Braintree::Configuration.public_key = "5t3rvbvhy8qtzqg5"
+Braintree::Configuration.private_key = "5f76514bdbcca81b3ca46ff66efe5b63"
