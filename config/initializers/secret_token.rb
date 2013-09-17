1d6a7c6ab41a2b4ac4cca50aa3ed368e7a0fab95
Andy Short <andynshort@gmail.com>
2013-09-17 10:46:55 -0700

    blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AresBilling::Application.config.secret_token = '2948a17b3b2e0cdcd9c7883b24a0fe06ead3754465a1d33c0aac574bf04f43658ebc4af9f7481c11bc5ca7264e0d454480d38ea6b7c45ac7d46ac9a21c31aa27'
