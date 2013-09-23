93ac39b2dd049f7d34c068e89bb813fd46ab3b77
Andy Short <andynshort@gmail.com>
2013-09-23 10:05:14 -0700

    added sync method to credit card model

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,4 +1,10 @@
 class CreditCard < ActiveRecord::Base
   attr_accessible :image_url, :last_4, :token, :user_id
   belongs_to :user
+
+  def sync bt_cc
+    options = { last_4: bt_cc.last_4, token: bt_cc.token }
+    options[:image_url] = bt_cc.image_url if bt_cc.image_url
+    update_attributes(options)
+  end
 end
